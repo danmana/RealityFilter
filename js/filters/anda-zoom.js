e8f7ae491a223d3c585d4d7d901baa5382b3ebae
Dan Manastireanu <Dan.Manastireanu@isdcnet.local>
2016-07-09 10:20:59 +0300

    Add eye tracking to anda zoom

--- js/filters/anda-zoom.js
+++ js/filters/anda-zoom.js
@@ -9,10 +9,48 @@ var map = document.createElement('canvas');
 	var filter = null;
 	var filter2 = null;
 
+
+
+var smoother = new Smoother([0.8, 0.8, 0.8, 0.8], [0, 0, 0, 0]);
+var detector = null;
+
+
 window.filters.push({
 	name : 'zoom',
 	author : 'anda',
 	draw : function (canvas, context) {
+
+	if (!detector) {
+                var width = ~~(60 * canvas.width / canvas.height);
+                var height  = 60;
+                detector = new objectdetect.detector(width, height, 1.1, objectdetect.frontalface_alt);
+            }
+
+            var coords = detector.detect(canvas, 1);
+            if (coords[0]) {
+                var coord = coords[0];
+                coord = smoother.smooth(coord);
+    console.log(coord);
+                // Rescale coordinates from detector to video coordinate space:
+                coord[0] *= canvas.width / detector.canvas.width;
+                coord[1] *= canvas.height / detector.canvas.height;
+                coord[2] *= canvas.width / detector.canvas.width;
+                coord[3] *= canvas.height / detector.canvas.height;
+    //console.log(coord);
+                // Display glasses overlay:
+                var rect = {};
+                rect.x    = ~~(coord[0] + coord[2] * 1.0/8 ) ;
+                rect.y     = ~~(coord[1] + coord[3] * 2.1/8 ) ;
+                rect.width   = ~~(coord[2] * 6/8);
+                rect.height  = ~~(coord[3] * 1.6/8);
+
+//            context.fillStyle = "rgba(0,255,0,0.4)";
+//            context.fillRect(rect.x, rect.y, rect.width, rect.height);
+
+            }
+
+
+
 
 		if (filter==null) {
 			filter = new DisplacementFilters.DisplacementMap(canvas, map, canvas, new DisplacementFilters.Point(), 15, 15,
@@ -24,26 +62,11 @@ window.filters.push({
 				DisplacementFilters.ColorChannel.RED, DisplacementFilters.ColorChannel.GREEN);
 		}
 
-		filter.point.x += 10;
-		filter.point.y += 10;
+		filter.point.x = Math.min(canvas.width, Math.max(0,rect.x + rect.width*0.33 - 125));
+		filter.point.y = Math.min(canvas.height, Math.max(0,rect.y + rect.height/2 - 125));
 
-		filter2.point.x -= 30;
-		filter2.point.y -= 30;
-		
-		if (filter.point.x > canvas.width) {
-			filter.point.x = 0;
-		}
-
-		if (filter.point.y > canvas.height) {
-			filter.point.y = 0;
-		}
-
-		if (filter2.point.x < 0 ) {
-			filter2.point.x = canvas.width;
-		}
-		if (filter2.point.y < 0) {
-			filter2.point.y = canvas.height;
-		}
+		filter2.point.x = Math.min(canvas.width, Math.max(0,rect.x + rect.width - 125));
+        filter2.point.y = Math.min(canvas.height, Math.max(0,rect.y + rect.height/2 - 125));
 
 		filter.scaleX = 80;
 		filter.scaleY = 80;
--- js/filters/eyes.js
+++ js/filters/eyes.js
@@ -1,28 +1,45 @@
 (function(){
 
+var smoother = new Smoother([0.995, 0.995, 0.995, 0.995], [0, 0, 0, 0]);
+var detector = null;
+
+
 window.filters.push({
 	name : 'eyes',
 	author : 'danmana',
 	draw : function (canvas, context) {
-	    // get the raw image data
-		var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-		var data = imageData.data;
+	    if (!detector) {
+            var width = ~~(60 * canvas.width / canvas.height);
+            var height  = 60;
+            detector = new objectdetect.detector(width, height, 1.1, objectdetect.frontalface_alt);
+        }
 
-        // ---------- Apply filter --------- //
-		for (var i = 0; i < data.length; i += 4) {
-			var red = data[i],
-                green = data[i + 1],
-                blue = data[i + 2];
+        var coords = detector.detect(canvas, 1);
+        if (coords[0]) {
+            var coord = coords[0];
+            coord = smoother.smooth(coord);
+console.log(coord);
+            // Rescale coordinates from detector to video coordinate space:
+            coord[0] *= canvas.width / detector.canvas.width;
+            coord[1] *= canvas.height / detector.canvas.height;
+            coord[2] *= canvas.width / detector.canvas.width;
+            coord[3] *= canvas.height / detector.canvas.height;
+//console.log(coord);
+            // Display glasses overlay:
+            var rect = {};
+            rect.x    = ~~(coord[0] + coord[2] * 1.0/8 ) ;
+            rect.y     = ~~(coord[1] + coord[3] * 2.1/8 ) ;
+            rect.width   = ~~(coord[2] * 6/8);
+            rect.height  = ~~(coord[3] * 1.6/8);
 
-            data[i] = 255 - red;
-            data[i+1] = 255 - green;
-            data[i+2] = 255 - blue;
-		}
+        context.fillStyle = "rgba(0,255,0,0.4)";
+        context.fillRect(rect.x, rect.y, rect.width, rect.height);
+
+        }
 
 
-        // paint the new data
-		imageData.data = data;
-		context.putImageData(imageData, 0, 0);
+
+
 	}
 });
 
