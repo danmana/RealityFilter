07c4e83b06df98d84768cb9a95358110d0402eef
andap <anda.plotuna@gmail.com>
2016-07-09 09:53:11 +0300

    Added zoom filter

--- js/filters/anda-contrasts.js
+++ js/filters/anda-contrasts.js
@@ -21,15 +21,18 @@ window.filters.push({
 		var factor = (259 * (contrast + 255)) / (255 * (259 - contrast));
 
         // ---------- Apply filter --------- //
+
 		for (var i = 0; i < data.length; i += 4) {
 			data[i] = factor * (data[i] - 128) + 128;
 			data[i+1] = factor * (data[i+1] - 128) + 128;
 			data[i+2] = factor * (data[i+2] - 128) + 128;
 		}
 
+
         // paint the new data
 		imageData.data = data;
 		context.putImageData(imageData, 0, 0);
+
 	}
 });
 
